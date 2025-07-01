328764d42315940c147fbc9be2129c4b424d35a6
nekomemepi <nekomemepi@gmail.com>
2025-07-01 18:44:11 +0100

    Use full path for assets and modules

--- .assets/view-mode.js
+++ .assets/view-mode.js
@@ -1,14 +1,18 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!-- PrismJS core -->
-<script src="../.assets/prism-core.js"></script>
-<link href="../.assets/prism-core.css" rel="stylesheet" />
+<!-- This must be the full path -->
+<base href="/path/to/your/notebook/directory/" />
 
-<script src="../.app/modules/Smiles/smiles-drawer.min.js"></script>
-<script src="../.app/modules/Smiles/smiles-adm-canvas.js"></script>
+<!-- PrismJS core -->
+<script src=".assets/prism-core.js"></script>
+<link href=".assets/prism-core.css" rel="stylesheet" />
+
+<!-- Import the smiles module -->
+<script src=".app/modules/Smiles/smiles-drawer.min.js"></script>
+<script src=".app/modules/Smiles/smiles-adm-canvas.js"></script>
 
 <!-- Custom CSS and JavaScript -->
-<link href="../.assets/inject-head.css" rel="stylesheet" />
-<script src="../.assets/inject-head.js"></script>
+<link href=".assets/inject-head.css" rel="stylesheet" />
+<script src=".assets/inject-head.js"></script>
   
